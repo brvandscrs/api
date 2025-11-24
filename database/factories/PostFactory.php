14d792c6bf983429cb64443ad22b3ee3ebd4f075
Bryan Descours <bryan.delianov@gmail.com>
2025-11-24 11:10:35 +0100

    ajout de model Post, migration et mise à jour du seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "slug" => $this->faker->unique()->slug(),
+            "title" => $this->faker->sentence(),
+            "content" => $this->faker->paragraphs(),
+            "statut" => $this->faker->boolean(),
+        ];
+    }
+}
